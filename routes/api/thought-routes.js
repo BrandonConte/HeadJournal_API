db1572df76869684adc3a341bd1ca90cd601f5e0
Brandon Conte <bconte950@gmail.com>
2022-01-26 17:40:56 -0500

    setup controllers and api routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,36 @@
+const router = require('express').Router();
+
+const { 
+    getAllThoughts, 
+    getThoughtById, 
+    createThought, 
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction
+
+} = require('../../controllers/thought-controller');
+
+// api/thoughts
+router.route('/')
+    .get(getAllThoughts);
+
+// api/thoughts/:id
+router.route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+// api/thoughts/:userId
+router.route('/:userId')
+ .post(createThought);
+
+// api/thoughts/:thoughId/reactions
+router.route('/:thoughtId/reactions')
+    .post(addReaction);
+
+// api/thoughts/:thoughtId/reactions
+router.route('/:thoughtId/reactions/reactionId')
+    .post(deleteReaction);
+
+module.exports = router;
