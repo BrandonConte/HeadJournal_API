c1ad8c654ae204964ff58bb493c42040eeb0a87c
Brandon Conte <bconte950@gmail.com>
2022-01-25 20:38:41 -0500

    reaction model setup

--- models/Thought.js
+++ models/Thought.js
@@ -1,5 +1,36 @@
-const { Schema, model } = require('mongoose');
+const { Schema, model, Types } = require('mongoose');
 const moment = require('moment');
+
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: ()=> new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 250
+        },
+        
+        username: {
+            type: String,
+            required: true
+        },
+
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => moment(createdAtVal).format('MMM DD, YYYY [at] hh:mm a')
+        }
+        },
+        {
+        toJSON: {
+            getters: true
+        } 
+        }
+    );
 
 const ThoughtSchema = new Schema(
     {
@@ -28,6 +59,9 @@ const ThoughtSchema = new Schema(
     id: false
     }
 )
+ThoughtSchema.virtual('reactionCount').get(function() {
+    return this.reactions.length;
+});
 
 const Thought = model('Thought', ThoughtSchema);
 
