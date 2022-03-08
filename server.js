8fffbc17084b2003e40d66b9afc431fc298935bc
Brandon Conte <bconte950@gmail.com>
2022-03-07 20:49:15 -0500

    Ok, last ones I swear

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.static('public'));
 
 app.use(require('./routes'));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/facespace', {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/headjournal', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
